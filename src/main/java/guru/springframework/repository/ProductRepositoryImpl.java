04ca2868d54d4dd6564470a1d562228c2003d011
John Thompson <fly.yamaha@gmail.com>
2015-06-06 09:31:03 -0400

    adding spring beans

--- src/main/java/guru/springframework/repository/ProductRepositoryImpl.java
+++ src/main/java/guru/springframework/repository/ProductRepositoryImpl.java
@@ -2,9 +2,6 @@ package guru.springframework.repository;
 
 import guru.springframework.model.Product;
 
-/**
- * Created by jt on 6/6/15.
- */
 public class ProductRepositoryImpl implements ProductRepository{
     @Override
     public Product getProduct(Long id){
